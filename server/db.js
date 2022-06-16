423de48474a43a4a62ae23d0da7c506e6d4c5b8b
Keshavaraj M <kesh3008@global.chainsys.com>
2022-06-16 10:01:55 +0530

    kr

--- server/db.js
+++ server/db.js
@@ -2,10 +2,16 @@ const Cloudant = require("@cloudant/cloudant");
 const { data } = require("./logger/logger");
 let url =
   "https://05025f1a-856b-47a0-aadb-52e737a386f3-bluemix.cloudantnosqldb.appdomain.cloud";
-let username = "apikey-v2-1kdtmo28t5uulevcbb5m8mifmj5bd962vbuc18qwa0m4";
-let password = "3589b77ff4cc367d60ae67e1f7dada03";
+let userCred = {
+  username: "apikey-v2-1kdtmo28t5uulevcbb5m8mifmj5bd962vbuc18qwa0m4",
+  password: "3589b77ff4cc367d60ae67e1f7dada03",
+};
 
-let cloudant = Cloudant({ url: url, username: username, password: password });
+let cloudant = Cloudant({
+  url: url,
+  username: userCred.username,
+  password: userCred.password,
+});
 
 let insert = function (paramsvalue) {
   console.log(paramsvalue);
